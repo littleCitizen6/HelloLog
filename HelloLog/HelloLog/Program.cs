34ce2af7270d122f28205c84716dd9a284987399
citizen6 <theLittleCitizen6@gmail.com>
2020-08-31 20:04:48 +0300

    finish part 1

---
+++ HelloLog/HelloLog/Program.cs
@@ -0,0 +1,18 @@
+﻿//using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace HelloLog
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Information("Hello Serilog!");
+        }
+    }
+}
